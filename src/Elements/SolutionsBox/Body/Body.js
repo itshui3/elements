722447f545e728d4a59113891cd4d31ac1551c67
Hui <jeffrey.x.hui@gmail.com>
2020-08-05 22:57:58 -0700

    solved code indentation issue

--- src/Elements/SolutionsBox/Body/Body.js
+++ src/Elements/SolutionsBox/Body/Body.js
@@ -13,8 +13,8 @@ const triangles = [
 
 const code = [
     'function nodeDepths(root, depth = 0) {',
-    '   if (root === null) return 0;',
-    '   return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);',
+    '\tif (root === null) return 0;',
+    '\treturn depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);',
     '}']
 
 
